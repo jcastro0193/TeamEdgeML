596c903b5a943a17f3ec2f637b27bd34e17013d2
jcastro0193 <jcastro2@nd.edu>
2020-02-19 11:30:21 -0500

    Clustered locations and extracted destinations

--- hmm.py
+++ hmm.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 from localization import Localization
+import math
 
 MAX_USERS = 1
 # Each file corresponds to an entire day, do we consider this one long path? What about different locations throughout the day?
@@ -31,14 +32,70 @@ def loadData():
                 # Convert last column to datetime, then extract H:M:S, then convert to timedelta, then seconds since 12 AM
                 data['time'] = pd.to_timedelta(pd.to_datetime(data['time']).dt.strftime("%H:%M:%S"))
                 data['time'] = data['time'].dt.total_seconds()
-                # TODO: Associate times to certain day of week or week, then cluster
-                data['user_id'] = int(user_id)
-                all_data = all_data.append(data)
+                all_data = all_data.append(data, ignore_index=True)
                 if cur_day == MAX_DAYS_PER_USER:
                     break
+    
     return all_data
 
+def myround(x, base):
+    return base * round(x/base)
+
+def cluster_points(df, base):
+    df['lat'] = df['lat'].apply(myround, args=(base,))
+    df['long'] = df['long'].apply(myround, args=(base,))
+    return df
+
+def show_locations(df):
+    for x, y in zip(df['long'].values.tolist(), df['lat'].values.tolist()):
+        plt.scatter(x, y)
+
+#    plt.scatter(df['lat'], df['long'])
+    plt.ylabel('locations')
+    plt.show()
+
+def find_destinations(df, length_of_stay):
+    length_of_stay *= 60 #convert to minutes
+    last_time = df['time'][0]
+    last_lat = df['lat'][0]
+    last_long = df['long'][0]
+    start_destination_index = 0
+    lats = []
+    longs = []
+    indices = []
+    for i in range(len(df['lat'])):
+        if df['lat'][i] != last_lat or df['long'][i] != last_long:
+            if df['time'][i-1] - last_time >= length_of_stay:
+                lats.append(last_lat)
+                longs.append(last_long)
+                indices.append(start_destination_index)
+            last_time = df['time'][i]
+            last_lat = df['lat'][i]
+            last_long = df['long'][i]
+            start_destination_index = i
+
+    destinations = pd.DataFrame()
+
+    destinations['long'] = longs
+    destinations['lat'] = lats
+
+    return destinations
+    
+def show_destinations(df):
+    plt.scatter(df['lat'], df['long'])
+    plt.ylabel('locations')
+    plt.show()
+
+
 def HMM(X):
+    # Cluster similar coordinates
+    X = cluster_points(X, .00005)
+    # show_locations(X)
+
+    # Find destinations based on 30 min stays
+    destinations_df = find_destinations(X, 1)
+    show_destinations(destinations_df)
+
     # Split 80/20
     train_set = X.sample(frac=0.8, random_state=0)
     test_set = X.drop(train_set.index)
@@ -51,8 +108,8 @@ def HMM(X):
 
     hidden_states = model.predict(test_set)
 
-    for i, state in enumerate(hidden_states):
-        print(i, state)
+#    for i, state in enumerate(hidden_states):
+#        print(i, state)
  
 
 if __name__ == "__main__":
